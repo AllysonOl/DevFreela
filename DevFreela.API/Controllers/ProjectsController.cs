7985a6d2055467e11e790a133fcf93a0b13b11e2
Allyson de Oliveira <allyson.ol@hotmail.com>
2023-02-28 18:43:51 -0300

    Add project files.

---
+++ DevFreela.API/Controllers/ProjectsController.cs
@@ -0,0 +1,92 @@
+﻿using DevFreela.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevFreela.API.Controllers
+{
+
+    [Route("api/projects")]
+    public class ProjectsController : ControllerBase
+    {
+        // api/projects?query=net core
+        [HttpGet]
+        public IActionResult Get(string query)
+        {
+            // Buscar todos ou filtrar
+
+            return Ok();
+        }
+
+        // api/projects/id
+        [HttpGet("{id}")]
+
+        public IActionResult GetById(int id)
+        {   
+            // Buscar o projeto
+            // return NotFound();
+
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] CreateProjectModel createProject)
+        {
+            if(createProject.Title.Length > 50)
+            {
+                return BadRequest();
+            }
+            
+            // Cadastrar o projeto
+
+            return CreatedAtAction(nameof(GetById), new { id = createProject.Id}, createProject);
+        }
+
+        // api/projects/2
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] UpdateProjectModel updateProject)
+        {
+            if(updateProject.Description.Length > 200) {
+                
+                return BadRequest();
+            }
+
+            return 
+                NoContent();    
+        }
+
+        // api/projects/3 DELETE
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            // Buscar, se não existir, retorna NotFound
+
+            //Remover 
+
+
+            return NoContent();
+        }
+
+        // api/projects/1/comments POST
+        [HttpPost("{id}/comments")]
+
+        public IActionResult PostComment(int id, [FromBody] CreateCommentModel createCommentModel)
+        {
+            return NoContent();
+        }
+
+        // api/projects/1/start
+        [HttpPut("{id}/start")]
+        public IActionResult Start(int id)
+        {
+            return NoContent();
+        }
+
+        // api/projects/1/finish
+        [HttpPut("{id}/finish")]
+        public IActionResult finish(int id)
+        {
+            return NoContent();
+        }
+
+
+    }
+}
